a9229d5fe215294b75423843d03ee896eafb3ea4
Heero04 <36018263+Heero04@users.noreply.github.com>
2022-10-18 00:23:27 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,4 +11,6 @@ option = streamlit.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
 
+streamlit.selectbox('OWN_A_PROPERTY',("N","Y"))
+
 streamlit.write('You selected:', option)
