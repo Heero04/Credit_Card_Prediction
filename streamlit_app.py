e286b589b621d2398f16092edbd1432b4e3ae50b
Heero04 <36018263+Heero04@users.noreply.github.com>
2022-10-18 00:10:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,10 @@ import pandas
 import requests
 
 
-streamlit.title('TEST 2')
-
-st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
-
-
+##streamlit.title('TEST 2')
 
 option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
 
 st.write('You selected:', option)
-##OWN_A_PROPERTY = st.selectbox('OWN_A_PROPERTY',("N","Y"))
