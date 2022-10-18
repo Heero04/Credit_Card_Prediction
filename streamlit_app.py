65e58916af2a8d86732204cc56f6db01ebd06a4b
Heero04 <36018263+Heero04@users.noreply.github.com>
2022-10-18 00:08:26 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,10 @@ import requests
 
 streamlit.title('TEST 2')
 
+st.selectbox(label, options, index=0, format_func=special_internal_function, key=None, help=None, on_change=None, args=None, kwargs=None, *, disabled=False, label_visibility="visible")
+
+
+
 option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
