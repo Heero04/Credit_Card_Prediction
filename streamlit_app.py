56ac804aeb6debb7ec5fcd17b7202178cc2b11c5
Heero04 <36018263+Heero04@users.noreply.github.com>
2022-10-18 00:15:46 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,6 @@ import requests
 
 # this is the main function in which we define our webpage 
 
-def main():       
-    # front end elements of the web page 
-    html_temp = """ 
-    <div style ="background-color:yellow;padding:13px"> 
-    <h1 style ="color:black;text-align:center;">Streamlit Loan Prediction ML App</h1> 
-    </div> 
-    """
 
 streamlit.title('TEST 2')
 
